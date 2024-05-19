0a4254de8db8806a76068d1a7691990feca0b8ed
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2024-05-19 21:16:19 +0530

    fixed cors error

--- src/main/java/com/org/projectmanager/config/AppConfig.java
+++ src/main/java/com/org/projectmanager/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.org.projectmanager.config;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,26 +37,19 @@ public class AppConfig {
       )
       .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
       .csrf(csrf -> csrf.disable())
-      .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+      .cors(Customizer.withDefaults());
     return http.build();
   }
 
-  private CorsConfigurationSource corsConfigurationSource() {
-    return new CorsConfigurationSource() {
-      @Override
-      public CorsConfiguration getCorsConfiguration(
-        HttpServletRequest request
-      ) {
-        CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(Collections.singletonList("*"));
-        cfg.setAllowedMethods(Collections.singletonList("*"));
-        cfg.setAllowCredentials(true);
-        cfg.setAllowedHeaders(Collections.singletonList("*"));
-        cfg.setExposedHeaders(Arrays.asList("Authorization"));
-        cfg.setMaxAge(3600L);
-        return cfg;
-      }
-    };
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final var configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(List.of("*"));
+    final var source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
--- src/main/java/com/org/projectmanager/config/JwtProvider.java
+++ src/main/java/com/org/projectmanager/config/JwtProvider.java
@@ -1,27 +1,36 @@
 package com.org.projectmanager.config;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import java.util.Date;
-
 import javax.crypto.SecretKey;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-
 @Configuration
 public class JwtProvider {
-    static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
-    public static String generateToken(final Authentication auth) {
-        return Jwts.builder().setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 8640000))
-                .claim("email", auth.getName()).signWith(key).compact();
-    }
+  static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
-    public String getEmailFromToken(String jwt) {
-        jwt = jwt.substring(7);
-        final var claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-        return String.valueOf(claims.get("email"));
-    }
+  public static String generateToken(final Authentication auth) {
+    return Jwts
+      .builder()
+      .setIssuedAt(new Date())
+      .setExpiration(new Date(new Date().getTime() + 8640000))
+      .claim("email", auth.getName())
+      .signWith(key)
+      .compact();
+  }
+
+  public String getEmailFromToken(String jwt) {
+    jwt = jwt.substring(7);
+    final var claims = Jwts
+      .parserBuilder()
+      .setSigningKey(key)
+      .build()
+      .parseClaimsJws(jwt)
+      .getBody();
+    return String.valueOf(claims.get("email"));
+  }
 }
